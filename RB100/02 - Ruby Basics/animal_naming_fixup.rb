489a93c4ae9ee61fea09dd7b131e59a3c6b78c95
Anthony Tibbs <anthony@trinimex.ca>
2021-05-25 13:00:16 -0400

    https://launchschool.com/exercises/dcfc3ed1

---
+++ RB100/02 - Ruby Basics/animal_naming_fixup.rb
@@ -0,0 +1,27 @@
+# Original (non-working code):
+
+# def dog
+#   return name
+# end
+
+# def cat(name)
+#   return name
+# end
+
+# puts "The dog's name is #{dog('Spot')}."
+# puts "The cat's name is #{cat}."
+
+
+# FIXED code:
+
+def dog(name)
+  return name
+end
+
+def cat(name)
+  return name
+end
+
+puts "The dog's name is #{dog('Spot')}."
+puts "The cat's name is #{cat('Ginger')}."
+
