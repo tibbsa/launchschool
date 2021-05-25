b7cb5c950aec820c0a3dd5892f780ea1e49f9d3e
Anthony Tibbs <anthony@trinimex.ca>
2021-05-25 13:03:15 -0400

    https://launchschool.com/exercises/27523561

---
+++ RB100/02 - Ruby Basics/multiply_sum.rb
@@ -0,0 +1,17 @@
+# Write the following methods so that each output is true 
+# puts add(2, 2) == 4
+# puts add(5, 4) == 9
+# puts multiply(add(2, 2), add(5, 4)) == 36
+
+
+def add(a, b)
+  a + b
+end
+
+def multiply(a, b)
+  a * b
+end
+
+puts add(2, 2) == 4
+puts add(5, 4) == 9
+puts multiply(add(2, 2), add(5, 4)) == 36
