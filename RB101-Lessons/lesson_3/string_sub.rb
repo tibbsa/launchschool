1d2ee6a5960a2899c4dd0fd2737bc3faaafd3dcf
Anthony Tibbs <anthony@trinimex.ca>
2021-05-31 21:22:33 -0400

    Move lesson notes and in-lesson assignments to private repo

--- RB101-Lessons/lesson_3/string_sub.rb
+++
@@ -1,8 +0,0 @@
-# https://launchschool.com/lessons/263069da/assignments/e2593fe1
-# Question 3
-# Replace the word "important" with "urgent" in this string:
-
-advice = "Few things in life are as important as house training your pet dinosaur."
-advice.sub!('important', 'urgent')
-
-puts advice
